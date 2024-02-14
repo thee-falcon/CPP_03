87c42b6b04e05318012948ef88d8d9ea423c4b41
thee-falcon <omar.makran.10@gmail.com>
2024-02-14 17:10:27 +0100

    the header file is done alhamdo-lilah

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -3,10 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:27:48 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/12 18:27:49 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/14 17:09:41 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+# include <iostream>
+
+class ClapTrap
+{
+private:
+    std::string     _name;
+    unsigned int    _hitPoints;
+    unsigned int    _energyPoints;
+    unsigned int    _attackPointes;
+public:
+    ClapTrap( const std::string& name );
+    ~ClapTrap();
+    // Copy constructor
+    ClapTrap(const ClapTrap& other);
+    // Assignment operator
+    ClapTrap& operator=(const ClapTrap& other);
+    void    attack(const std::string& target);
+    void    takeDamage(unsigned int amount);
+    void    beRepaired(unsigned int amount);
+};
+
+#endif
