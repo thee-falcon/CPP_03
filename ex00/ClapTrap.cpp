27ac2ae090f79e016c8e7bb5a85971de29f60f82
thee-falcon <omar.makran.10@gmail.com>
2024-02-14 17:23:07 +0100

    edit headerFile, is done alhamdo-lilah

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -3,10 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:27:43 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/12 18:27:44 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/14 17:20:22 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( const std::string& name ) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+    
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:27:48 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/14 17:09:41 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/14 17:21:44 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
 
 class ClapTrap
 {
-private:
     std::string     _name;
-    unsigned int    _hitPoints;
+    unsigned int    _hitPoints; // represent the health of the ClapTrap
     unsigned int    _energyPoints;
-    unsigned int    _attackPointes;
+    unsigned int    _attackDamage;
 public:
     ClapTrap( const std::string& name );
     ~ClapTrap();
