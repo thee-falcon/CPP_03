c7e7c42784287aa052104674e1133ea41e569797
Omar Makran <omakran@c1r2p3.1337.ma>
2024-02-15 21:55:30 +0100

    the file ScavTrapCPP is going on

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,8 +6,27 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 19:25:46 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/15 21:30:47 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/15 21:49:27 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
+
+// constructor definition
+ScavTrap::ScavTrap( const std::string& name ) : ClapTrap( name ) {
+    // Initializing attributes with ScavTrap's values
+    this->_hitPoints = 100;
+    this->_energyPoints = 50;
+    this->_attackDamage = 20;
+    
+    std::cout << "ScavTrap " << this->_name << " is born! Ready for some scavenging!" << std::endl;
+}
+
+// destructor definition
+ScavTrap::~ScavTrap() {
+    std::cout << "ScavTrap " << this->_name << " bids farewell. Gatekeeper mode deactivated." << std::endl;
+}
+
+void    ScavTrap::guardGate() {
+    std::cout << "ScavTrap " << this->_name << " is now in Gatekeeper mode. Guarding the gate!" << std::endl;
+}
