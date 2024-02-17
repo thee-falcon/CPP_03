b2be6c9a428f0d048fad5585da4d407eaffb9fb5
Omar Makran <omakran@c1r2p3.1337.ma>
2024-02-17 17:17:16 +0100

    All checked, ex01 is done alhamdo-lilah

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:27:55 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/17 16:07:40 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/17 17:14:48 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ int main() {
     ScavTrap scavtrapcopy(scavtrap);
 
     // Test original ScavTrap
-    std::cout << "------------- Test Original ScavTrap ------------" << std::endl;
+    std::cout << "------------- Test Original ScavTrap ------------\n" << std::endl;
     scavtrap.attack("Target 1");
     scavtrap.takeDamage(3);
     scavtrap.beRepaired(10);
     scavtrap.guardGate();
 
     // Test copied ScavTrap
-    std::cout << "------------ Test Copied ScavTrap ---------------" << std::endl;
+    std::cout << "------------ Test Copied ScavTrap ---------------\n" << std::endl;
     scavtrapcopy.attack("Target 2");
     scavtrapcopy.takeDamage(5);
     scavtrapcopy.beRepaired(8);
     scavtrapcopy.guardGate();
     
-    return 0;
+    return (0);
 }
